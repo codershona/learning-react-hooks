977aa3d8bb906aeb9ce5c0d4f0b7089d38d1577b
codershona <falgunimst95@gmail.com>
2020-04-27 18:13:33 +0200

    Using the Custom Hooks : method 2 done

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import Card from '../UI/Card';
 import './Search.css';
 
+import useHttp from '../../hooks/http';
+
+import ErrorModal from '../UI/ErrorModal';
+
 const Search = React.memo(props => {
 
   const { onLoadIngredients } = props;
@@ -10,6 +14,8 @@ const Search = React.memo(props => {
   const [enteredFilter, setEnteredFilter] = useState('');
   
   const inputRef = useRef();
+
+  const { isLoading, data, error, sendRequest, clear } = useHttp();
 
    useEffect(() => {
 
@@ -22,31 +28,30 @@ const Search = React.memo(props => {
     ? '' 
     : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-
-  fetch('https://learning-react-hooks-fa290.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-     .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-
-        });
-
-      }
-
-   // setUserIngredients(loadedIngredients);
-   // ...
-    onLoadIngredients(loadedIngredients);
+   sendRequest('https://learning-react-hooks-fa290.firebaseio.com/ingredients.json' + query, 'GET');
 
 
-           });
-   
+  // fetch('https://learning-react-hooks-fa290.firebaseio.com/ingredients.json' + query)
+  //   .then(response => response.json())
+  //    .then(responseData => {
 
-       }
+  //        const loadedIngredients = [];
+  //     for (const key in responseData) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: responseData[key].title,
+  //         amount: responseData[key].amount
 
+  //       })
+
+  //     }
+
+  //   onLoadIngredients(loadedIngredients);
+
+  // }
+
+
+    }
 
 
     }, 500);
@@ -58,18 +63,42 @@ const Search = React.memo(props => {
     };
 
 
-   }, [enteredFilter, onLoadIngredients, inputRef]);
+   }, [enteredFilter, inputRef, sendRequest]);
+
+   useEffect(() => {
+    if (!isLoading && !error && data) {
+
+              const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+
+        })
+
+      }
+
+    onLoadIngredients(loadedIngredients);
+
+      }
+
+   }, [data, isLoading, error, onLoadIngredients]);
 
 
 
   return (
     <section className="search">
 
+    {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
       <Card>
 
         <div className="search-input">
 
           <label>Filter by Title</label>
+
+          {isLoading && <span>Loading...</span>}
 
           <input
 
